ea30b3472d5e876fa038131a24305570ec7e6a99
pedrokimura <pedrok@grupopanvel.com.br>
2024-09-21 17:56:00 -0300

    add boilerplate

---
+++ src/test/java/com/pedrok/pockafka/PocKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrok.pockafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
